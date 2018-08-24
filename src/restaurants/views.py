5b040d648d23f8ebe333c2cb08d898e2467c493b
carlfvarela <carlfvarela@gmail.com>
2018-08-24 17:23:29 +0100

    2h30 into tutorial

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,34 +1,32 @@
-import random
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView, ListView
+from .models import RestaurantLocation
 
-#function based view
-def home(request):
-	num = None
-	some_list = [
-		random.randint(0,10000),
-		random.randint(0,10000),
-		random.randint(0,10000)
-		]
-	condition_bool_item = True
-
-	if condition_bool_item:
-		num = random.randint(0,10000)
-
+def restaurant_listview(request):
+	template_name = 'restaurants/restaurants_list.html'
+	queryset = RestaurantLocation.objects.all()
 	context = {
-		"bool_item": False, 
-		"num":num, 
-		"some_list":some_list
+		"object_list": queryset
 	}
-	return render(request,"home.html", context)
-
-def about(request):
-	context = {
-	}
-	return render(request,"about.html", context)
+	return render(request, template_name, context)
 
 
-def contact(request):	
-	context = {
-	}
-	return render(request,"contact.html", context)
+
+class RestaurantListView(ListView):
+	queryset = RestaurantLocation.objects.all()
+	template_name = 'restaurants/restaurants_list.html'
+class SearchRestaurantListView(ListView):
+	
+	template_name = 'restaurants/restaurants_list.html'
+	def get_queryset(self):
+		print(self.kwargs)
+		slug = self.kwargs.get("slug")
+		if slug:
+			queryset = RestaurantLocation.objects.filter(Q(category_iexact=slug) | Q(category_icontains=slug))
+		else:
+			queryset = RestaurantLocation.objects.none()
+		return queryset 
+
