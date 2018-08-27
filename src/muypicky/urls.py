4a0a08b41b6cee1861999d6c48d64ecae88b2b18
carlfvarela <carlfvarela@gmail.com>
2018-08-27 16:40:19 +0100

    3h33 into tutorial

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+
 from restaurants.views import (
     restaurant_listview,
     RestaurantListView,
-    SearchRestaurantListView,
+    RestaurantDetailView,
+    restaurant_createview,
 )
 
 
@@ -27,8 +29,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^restaurants/$', RestaurantListView.as_view()),
-    url(r'^restaurants/(?P<slug>\w+)/$', SearchRestaurantListView.as_view()),
-    #url(r'^restaurants/asian/$', AsianFusionRestaurantListView.as_view()),
+    url(r'^restaurants/create/$', restaurant_createview),
+    url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,9 +1,30 @@
 from django.db.models import Q
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import RestaurantLocation
+from .forms import RestaurantCreateForm
+
+
+def restaurant_createview(request):
+	if request.method == 'POST':
+		form = RestaurantCreateForm(request.POST)
+		if form.is_valid():
+			obj = RestaurantLocation.objects.create(
+				name = form.cleaned_data.get('name'),
+				location = form.cleaned_data.get('location'),
+				category = form.cleaned_data.get('category')
+				)
+			return HttpResponseRedirect("/restaurants/")
+
+		if form.errors:
+			print(form.errors)	
+
+	template_name = 'restaurants/form.html'
+	context = {}
+	return render(request, template_name, context)
+
 
 def restaurant_listview(request):
 	template_name = 'restaurants/restaurants_list.html'
@@ -14,19 +35,31 @@ def restaurant_listview(request):
 	return render(request, template_name, context)
 
 
-
-class RestaurantListView(ListView):
+def restaurant_detailview(request):
+	template_name = 'restaurants/restaurants_list.html'
 	queryset = RestaurantLocation.objects.all()
-	template_name = 'restaurants/restaurants_list.html'
-class SearchRestaurantListView(ListView):
-	
-	template_name = 'restaurants/restaurants_list.html'
+	context = {
+		"object_list": queryset
+	}
+	return render(request, template_name, contex)
+
+
+class RestaurantListView(ListView): 
 	def get_queryset(self):
-		print(self.kwargs)
 		slug = self.kwargs.get("slug")
 		if slug:
-			queryset = RestaurantLocation.objects.filter(Q(category_iexact=slug) | Q(category_icontains=slug))
+			queryset = RestaurantLocation.objects.filter(
+				Q(category__icontains=slug) |
+				Q(category__iexact=slug)
+			)
 		else:
-			queryset = RestaurantLocation.objects.none()
+			queryset = RestaurantLocation.objects.all()
 		return queryset 
 
+
+class RestaurantDetailView(DetailView): 
+	queryset = RestaurantLocation.objects.all()
+	#def get_object(self, *args, **kwargs):
+	#	rest_id = self.kwargs.get('rest_id')
+	#	obj = get_object_or_404(RestaurantLocation, id = rest_id)
+	#	return obj
